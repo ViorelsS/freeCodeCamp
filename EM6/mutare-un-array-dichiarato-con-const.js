601f1d205129b729378a1581e1f6bac78bfeccaf
ViorelsS <55164639+ViorelsS@users.noreply.github.com>
2021-11-20 23:19:09 +0100

    Primi esercizi EM6

---
+++ EM6/mutare-un-array-dichiarato-con-const.js
@@ -0,0 +1,12 @@
+const s = [5, 7, 2];
+function editInPlace() {
+  // Modifica il codice solo sotto questa riga
+
+  // Utilizzare s = [2, 5, 7] non sarebbe valido
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+  // Modifica il codice solo sopra questa riga
+}
+editInPlace();
